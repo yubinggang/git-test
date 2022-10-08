d06b1364926cf2dd5207240062dc888e7751a586
yubinggang <90540404+yubinggang@users.noreply.github.com>
2022-10-08 11:25:08 +0800

    Update GitTest.java

--- src/main/java/com/yu/git/GitTest.java
+++ src/main/java/com/yu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello master test");
         System.out.println("hello hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
