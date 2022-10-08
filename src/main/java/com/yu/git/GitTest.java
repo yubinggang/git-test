f141ac2532e46b22bad19e4d1b9248fab2c420fc
yu <2535702734@qq.com>
2022-10-08 10:46:01 +0800

    hotfix second commit

--- src/main/java/com/yu/git/GitTest.java
+++ src/main/java/com/yu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello update");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix test");
+
     }
 }
